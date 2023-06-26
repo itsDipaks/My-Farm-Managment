8f63fe18bb8df66d9b00e924bcb9fad0caf5693d
Dipak Pawar <itsdipakpawar4206@gmail.com>
2023-06-26 13:20:42 +0530

    Project Updated

--- CropManagment/src/app/app.module.ts
+++ CropManagment/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BottomNavComponent } from './Shared/Components/bottom-nav/bottom-nav.component';
+import { TopNavComponent } from './Shared/Components/top-nav/top-nav.component';
+ 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BottomNavComponent,
+    TopNavComponent
   ],
   imports: [
     BrowserModule,
